b210dda0143983eb4c1f58d549f5e47ad02185bf
MaryamSaleemm <maryamsaleem176@gmail.com>
2025-07-13 06:47:44 -0400

    Revert "Revert "Revert "Update Shipping_Cost_Calculator.py"""
    This reverts commit 1f60baedb378eeeee7fcadaa9bc7bf875098ef77.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -12,3 +12,6 @@ print(f"Shipping Cost: {shipping_cost} USD")
  # Here is a new update by MaryamSaleemm
 # Here is another update by MAryamSaleemm
 
+# File added by Sklup55
+
+ # Here is a new update by Sklup55
